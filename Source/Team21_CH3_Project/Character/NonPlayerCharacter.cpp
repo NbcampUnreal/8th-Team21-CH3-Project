88a9f4eeb498e5c7a159696191cb2691ef5e3eed
LAPTOP-TANPRLPA\huil3 <huil33@naver.com>
2026-05-06 02:18:50 +0900

    [이후일] AI 기본구현

--- Source/Team21_CH3_Project/Character/NonPlayerCharacter.cpp
+++ Source/Team21_CH3_Project/Character/NonPlayerCharacter.cpp
@@ -5,8 +5,9 @@
 
 #include "Controller/AI_Controller.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Animation/AnimInstance.h"
 
-ANonPlayerCharacter::ANonPlayerCharacter()
+ANonPlayerCharacter::ANonPlayerCharacter() : bIsNowAttacking(false)
 {
 	PrimaryActorTick.bCanEverTick = true;
 
@@ -25,5 +26,39 @@ void ANonPlayerCharacter::BeginPlay()
 		GetCharacterMovement()->bOrientRotationToMovement = false;
 		GetCharacterMovement()->bUseControllerDesiredRotation = true;
 		GetCharacterMovement()->RotationRate = FRotator(0.f, 480.f, 0.f);
+	}
+}
+
+void ANonPlayerCharacter::BeginAttack()
+{
+	UAnimInstance* AnimInstance = Cast<UAnimInstance>(GetMesh()->GetAnimInstance());
+	checkf(IsValid(AnimInstance) == true, TEXT("Invalid AnimInstance"));
+
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+	/*if (IsValid(AnimInstance) == true && IsValid(AttackMeleeMontage) == true && AnimInstance->Montage_IsPlaying(AttackMeleeMontage) == false)
+	{
+		AnimInstance->Montage_Play(AttackMeleeMontage);
+
+		bIsNowAttacking = true;
+
+		if (OnAttackMontageEndedDelegate.IsBound() == false)
+		{
+			OnAttackMontageEndedDelegate.BindUObject(this, &ThisClass::EndAttack);
+			AnimInstance->Montage_SetEndDelegate(OnAttackMontageEndedDelegate, AttackMeleeMontage);
+		}
+	}
+	*/
+	
+}
+
+void ANonPlayerCharacter::EndAttack(UAnimMontage* InMontage, bool bInterruped)
+{
+	GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+
+	bIsNowAttacking = false;
+
+	if (OnAttackMontageEndedDelegate.IsBound() == true)
+	{
+		OnAttackMontageEndedDelegate.Unbind();
 	}
 }
