541c4a0087a5a5077e2373e3a2cb368b1e0db48c
Austin\AustinKim <dong961125@gmail.com>
2026-05-06 01:55:56 +0900

    [김동연] [Jump구현, Animation 구현]
    InputConfig, CharacterAnimInstance 생성

---
+++ Source/Team21_CH3_Project/Animation/CharacterAnimInstance.cpp
@@ -0,0 +1,34 @@
+
+
+#include "Animation/CharacterAnimInstance.h"
+#include "Character/CharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h" //여러 수학적 로직을 가지고있는 Static 클래스
+
+void UCharacterAnimInstance::NativeInitializeAnimation()
+{
+	APawn* OwnerPawn = TryGetPawnOwner();
+
+	if (IsValid(OwnerPawn))
+	{
+		OwnerCharacter = Cast<ACharacterBase>(OwnerPawn);
+		OwnerCharacterMovement = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (IsValid(OwnerCharacter) && IsValid(OwnerCharacterMovement)) //캐릭터가 있고, movementcomponent도 있다면
+	{
+		Velocity = OwnerCharacterMovement->Velocity;
+
+		GroundSpeed = UKismetMathLibrary::VSizeXY(Velocity);
+		// Z는 무시하고XY값만 사용
+		// 점프 중에도 XY값만 적용
+
+		bIsMove = (KINDA_SMALL_NUMBER < GroundSpeed);
+		bIsFalling = OwnerCharacterMovement->IsFalling();
+	}
+
+	
+}
--- Source/Team21_CH3_Project/Input/InputConfig.h
+++ Source/Team21_CH3_Project/Input/InputConfig.h
@@ -18,5 +18,6 @@ public:
 	TObjectPtr<UInputAction> Move;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TObjectPtr<UInputAction> Look;
-
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Jump;
 };
