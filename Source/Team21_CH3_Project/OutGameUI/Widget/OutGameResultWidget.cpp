d72646854f9e2b9a9c242869618664f4af6b99ee
pro0518 <justmusic0518@gmail.com>
2026-05-06 10:06:30 +0900

    [조범근][기능개선]

--- Source/Team21_CH3_Project/OutGameUI/Widget/OutGameResultWidget.cpp
+++ Source/Team21_CH3_Project/OutGameUI/Widget/OutGameResultWidget.cpp
@@ -1,2 +1,13 @@
 // OutGameResultWidget.cpp
 #include "OutGameUI/Widget/OutGameResultWidget.h"
+#include "Components/Button.h"
+
+void UOutGameResultWidget::NativeOnInitialized(){
+	Super::NativeOnInitialized();
+	
+	if (IsValid(ReturnToLobbyButton) == true) ReturnToLobbyButton->OnClicked.AddUniqueDynamic(this, &ThisClass::HandleReturnToLobby);
+}
+
+void UOutGameResultWidget::HandleReturnToLobby(){
+	
+}
