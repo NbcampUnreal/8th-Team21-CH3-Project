13b4d3e37a32058e100661810b8f38633245b9c7
pro0518 <justmusic0518@gmail.com>
2026-05-06 12:52:46 +0900

    [조범근][InGameGameMode 추가, 파일 정리]

---
+++ Source/Team21_CH3_Project/InGameUI/Game/InGameGameMode.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "InGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM21_CH3_PROJECT_API AInGameGameMode : public AGameMode{
+	GENERATED_BODY()
+};
